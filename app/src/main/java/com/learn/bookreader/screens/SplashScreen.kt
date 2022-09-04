54e33e73b442fedef66a586c86d9095cffa10302
mvipe <mohnishvats@gmail.com>
2022-09-04 07:25:25 +0530

    designed the splash screen

--- app/src/main/java/com/learn/bookreader/screens/SplashScreen.kt
+++ app/src/main/java/com/learn/bookreader/screens/SplashScreen.kt
@@ -1,10 +1,44 @@
 package com.learn.bookreader.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 
+@Preview
 @Composable
-fun SplashScreen(navController: NavHostController) {
-    Text(text = "Splash Screen")
+fun SplashScreen(navController: NavController=NavController(context = LocalContext.current)) {
+    Surface(modifier = Modifier
+        .padding(15.dp)
+        .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width=2.dp, color = Color.LightGray),
+
+    ) {
+        Column(modifier = Modifier.padding(1.dp)
+            , horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+        Text(text = "Book Reader", style = MaterialTheme.typography.headlineMedium
+        , color = Color.Red.copy(alpha = 0.5f))
+            
+            Spacer(modifier = Modifier.height(15.dp))
+            
+            Text(text = "\"Read. Complete. Repeat\"",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+
+    }
 }
