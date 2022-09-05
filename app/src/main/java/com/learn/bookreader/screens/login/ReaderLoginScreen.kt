7d3949d1acee0ab8757c4c6aff04d57fffcf5284
mvipe <mohnishvats@gmail.com>
2022-09-06 05:03:23 +0530

    setting up the login screen

--- app/src/main/java/com/learn/bookreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/learn/bookreader/screens/login/ReaderLoginScreen.kt
@@ -1,15 +1,19 @@
 package com.learn.bookreader.screens.login
 
+import android.graphics.Outline
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -17,10 +21,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.learn.bookreader.components.EmailInput
+import com.learn.bookreader.components.PasswordInput
 import com.learn.bookreader.components.ReaderLogoText
 
 
@@ -30,6 +43,10 @@ import com.learn.bookreader.components.ReaderLogoText
             Column(horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Top) {
                 ReaderLogoText()
+                UserForm(loading = false, isCreateAccount = false){
+                    email,password ->
+                    Log.d("email pa",email+" "+password)
+                }
             }
         }
 
@@ -38,7 +55,11 @@ import com.learn.bookreader.components.ReaderLogoText
 @OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
-fun UserForm(){
+fun UserForm(
+    loading:Boolean=false,
+    isCreateAccount:Boolean=false,
+    onDone: (String,String) -> Unit={email,pwd->}
+){
     val email= rememberSaveable() {
         mutableStateOf("")
     }
@@ -63,9 +84,27 @@ fun UserForm(){
         .verticalScroll(rememberScrollState())
     
     Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally,) {
-        
+        EmailInput(emailState = email, enabled = !loading, onAction = KeyboardActions{passwordFocusRequest.requestFocus()})
+
     }
+
+    PasswordInput(modifier=modifier.focusRequester(passwordFocusRequest)
+        ,passwordState=password,
+        labelId="Password",
+
+        enabled=!loading,
+        passwordVisibility=passwordVisibility,
+        onAction=KeyboardActions {
+            if(!valid) return@KeyboardActions
+            // onDone(email.value.trim() , password.value.trim())
+        },
+
+        )
+
+
+
 
 
 
 }
+
