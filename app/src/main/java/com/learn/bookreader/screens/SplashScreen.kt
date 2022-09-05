622490c38f7ce8cfd846f77c995128f2ad540af2
mvipe <mohnishvats@gmail.com>
2022-09-05 06:23:02 +0530

    animate the splash screen

--- app/src/main/java/com/learn/bookreader/screens/SplashScreen.kt
+++ app/src/main/java/com/learn/bookreader/screens/SplashScreen.kt
@@ -1,5 +1,9 @@
 package com.learn.bookreader.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -7,21 +11,41 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import kotlinx.coroutines.delay
 
 @Preview
 @Composable
 fun SplashScreen(navController: NavController=NavController(context = LocalContext.current)) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true ){
+        scale.animateTo(targetValue = 0.9f,
+                animationSpec = tween(durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(8f)
+                        .getInterpolation(it)
+                }))
+
+        delay(2000L)
+    }
+    
+    
     Surface(modifier = Modifier
         .padding(15.dp)
-        .size(330.dp),
+        .size(330.dp)
+        .scale(scale.value),
         shape = CircleShape,
         color = Color.White,
         border = BorderStroke(width=2.dp, color = Color.LightGray),
